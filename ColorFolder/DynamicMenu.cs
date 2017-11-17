04e45b5b79c8f8bccdd7a32125783c88ecc8f60f
Bence Eros <benirave@gmail.com>
2017-11-17 19:15:45 +0100

    commit

---
+++ ColorFolder/DynamicMenu.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ColorFolder.Properties;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+
+
+namespace ColorFolder
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.AllFiles)]
+    [COMServerAssociation(AssociationType.Directory)]
+    class DynamicMenu : SharpContextMenu
+    {
+        private ContextMenuStrip menu = new ContextMenuStrip();
+
+        protected override bool CanShowMenu()
+        {
+            //  We can show the item only for a single selection.
+            if (SelectedItemPaths.Count() == 1)
+            {
+                this.UpdateMenu();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            menu.Items.Clear();
+            FileAttributes attr = File.GetAttributes(SelectedItemPaths.First());
+
+            // check if the selected item is a directory
+            if (attr.HasFlag(FileAttributes.Directory))
+            {
+                this.MenuDirectory();
+            }
+            else
+            {
+                //EErosrothis.MenuFiles();
+            }
+
+            // return the menu item
+            return menu;
+        }
+
+        // <summary>
+        // Updates the context menu. 
+        // </summary>
+        private void UpdateMenu()
+        {
+            // release all resources associated to existing menu
+            menu.Dispose();
+            menu = CreateMenu();
+        }
+
+        protected void MenuDirectory()
+        {
+            ToolStripMenuItem MainMenu;
+            MainMenu = new ToolStripMenuItem
+            {
+                Text = "Color",
+                //Image = Properties.Resources.blue
+            };
+
+            ToolStripMenuItem SubMenu1;
+            SubMenu1 = new ToolStripMenuItem
+            {
+                Text = "Green",
+                Image = Properties.Resources.folder__55_
+            };
+
+            var SubMenu2 = new ToolStripMenuItem
+            {
+                Text = "Yellow",
+                Image = Properties.Resources.folder__62_
+            };
+            SubMenu2.DropDownItems.Clear();
+            SubMenu1.Click += (sender, args) => ApplyFolderIcon(53);
+            SubMenu2.Click += (sender, args) => ApplyFolderIcon(60);
+            MainMenu.DropDownItems.Add(SubMenu1);
+            MainMenu.DropDownItems.Add(SubMenu2);
+
+            menu.Items.Clear();
+            menu.Items.Add(MainMenu);
+        }
+
+        private void ApplyFolderIcon(int icon)
+        {
+            foreach (var SelectedItem in SelectedItemPaths)
+            {
+                var iniPath = Path.Combine(SelectedItem, "desktop.ini");
+
+
+                if (File.Exists(iniPath))
+                {
+                    //remove hidden and system attributes to make ini file writable
+                    File.SetAttributes(
+                        iniPath,
+                        File.GetAttributes(iniPath) &
+                        ~(FileAttributes.Hidden | FileAttributes.System));
+                }
+
+                //create new ini file with the required contents
+                var iniContents = new StringBuilder()
+                    .AppendLine("[.ShellClassInfo]")
+                    .AppendLine($"IconResource={@"C:\Users\Beni\Documents\CFIcons.dll"},{icon}")
+                    .ToString();
+                File.WriteAllText(iniPath, iniContents);
+
+                //hide the ini file and set it as system
+                File.SetAttributes(
+                    iniPath,
+                    File.GetAttributes(iniPath) | FileAttributes.Hidden | FileAttributes.System);
+                //set the folder as system
+                File.SetAttributes(
+                    SelectedItem,
+                    File.GetAttributes(SelectedItem) | FileAttributes.System);
+
+                //refreshUI.Refresh();
+            }
+        }
+    }
+}
---
+++ ColorFolder/refreshUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorFolder
+{
+    public class refreshUI
+    {
+        [DllImport("user32")]
+        private static extern int PostMessage(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32")]
+        private static extern IntPtr FindWindow(string className, string caption);
+        [DllImport("user32")]
+        private static extern IntPtr FindWindowEx(IntPtr parent, IntPtr startChild, string className, string caption);
+        public static void Refresh()
+        {
+            IntPtr d = FindWindow("Progman", "Program Manager");
+            d = FindWindowEx(d, IntPtr.Zero, "SHELLDLL_DefView", null);
+            d = FindWindowEx(d, IntPtr.Zero, "SysListView32", null);
+            PostMessage(d, 0x100, new IntPtr(0x74), IntPtr.Zero);//WM_KEYDOWN = 0x100  VK_F5 = 0x74
+            PostMessage(d, 0x101, new IntPtr(0x74), new IntPtr(1 << 31));//WM_KEYUP = 0x101
+        }
+    }
+}
